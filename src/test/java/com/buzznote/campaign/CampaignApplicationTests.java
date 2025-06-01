11ee64162b5a17958a889d70b8dcc1af091a8819
Sahan <kksahan78@gmail.com>
2025-06-01 11:39:40 +0530

    Initial commit

---
+++ src/test/java/com/buzznote/campaign/CampaignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buzznote.campaign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampaignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
